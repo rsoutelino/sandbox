91c9e2f8b2c5400b8d8b6aa07dd0d512e5445aa4
Rafael Soutelino <rsoutelino@gmail.com>
2015-08-09 18:09:31 +1200

    dealing with empty pre-categorized spreasheet

--- finances.py
+++ finances.py
@@ -1,7 +1,12 @@
-import os, yaml
+import os, sys,  yaml
 from glob import glob
 import numpy as np
-import pandas as pd 
+import pandas as pd
+import logging 
+
+# TO-DO list
+#     - put wildcards for things that enumerate: YouTube, Tank
+#     - hook directly to google drive instead of keeping exporting
 
 pd.set_option('display.max_rows', 10)
 pd.set_option('expand_frame_repr', False)
@@ -15,12 +20,15 @@ class MonthStatement(object):
 
         self.data = os.path.join(DATADIR, 'statement_%s%02d.csv' %(year, month))
         self.df = pd.read_csv(self.data, index_col=0)
-        # self.categorize()
+        self.categorize()
 
     def categorize(self):
         # removing ingnorables
-        self.df = self.df[self.df.category != 'ignore']
-        self.df = self.df.fillna(-999)
+        try:
+            self.df = self.df[self.df.category != 'ignore']
+            self.df = self.df.fillna(-999)
+        except TypeError:
+            self.df.category = -999
 
         self.df['category2'] = -999
 
@@ -44,19 +52,31 @@ class MonthStatement(object):
         c1, c2 = [], []
         # discovering categories based on beneficiaries
         # allindexes, indexes = [], []
+        notFoundList = False
         for idx, ben in enumerate(self.df.beneficiary):
+            foundBen = False
             if idx not in saveidx:
                 # allindexes.append(idx)
                 for key1, dict1 in self.categories.items():
                     for key2, value2 in dict1.items():
                         if ben in value2:
+                            foundBen = True
                             # indexes.append(idx)
                             # print idx, " Beneficiary %s is contained in cat2 = %s" %(ben, key2)
                             c1.append(key1)
                             c2.append(key2)
             else:
+                foundBen = True
                 c1.append(self.df.category[idx])
                 c2.append(self.df.category2[idx])
+
+            if not foundBen:
+                notFoundList = True
+                logging.error('No category found. Categorize "%s" and retry.' %ben)
+
+
+        if notFoundList:
+            sys.exit('Categorization FAILED. Categorize the items above and retry.')
 
         self.df.category = c1
         self.df.category2 = c2
