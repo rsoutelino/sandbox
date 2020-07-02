8c6a513f2a6f8cb22b0f2b2be63473aa9aa9fe39
Rafael Soutelino <r.soutelino@oceanum.science>
2020-07-03 09:42:20 +1200

    add quick point plot based on folium

---
+++ plotting/quickPointPlot.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import os, argparse
+import numpy as np
+import folium
+
+from mpl_toolkits.basemap import Basemap
+
+class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
+    """Hack to allow showing default values in help and preserving line break in epilog"""
+    pass
+
+parser = argparse.ArgumentParser(description='Currents validation.',
+                                 formatter_class=CustomFormatter)
+
+parser.add_argument('-x', '--lons', type=float, nargs='+',
+                   help='List of longitudes. Ex: [-44 -45.3 -41]')
+
+parser.add_argument('-y', '--lats', type=float, nargs='+',
+                   help='List of latitudes: Ex: [-23 -34 -28]')
+
+parser.add_argument('-r', '--resolution', type=str, default='i',
+                   help='Coastline resolution: Ex: [i, h, f, l]')
+
+args = parser.parse_args()
+print("\n")
+
+m = folium.Map(location=[np.mean(args.lats), np.mean(args.lons)], zoom_start=2)
+
+for lon, lat in zip(args.lons, args.lats):
+    folium.Marker( location=[lat, lon]).add_to(m)
+
+m.save("/tmp/map.html")
+
+os.system("google-chrome /tmp/map.html")
+
+
