49e22ea1dcd8c54e67e11e27f605086e1c4c2178
Rafael Soutelino <r.soutelino@metocean.co.nz>
2021-10-11 16:25:13 +1300

    remove unsed

--- plotting/quickPointPlot.py
+++ plotting/quickPointPlot.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python
-import os, argparse
+import os
+import argparse
 import numpy as np
 import folium
 
-from mpl_toolkits.basemap import Basemap
 
 class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
     """Hack to allow showing default values in help and preserving line break in epilog"""
     pass
 
+
 parser = argparse.ArgumentParser(description='Currents validation.',
                                  formatter_class=CustomFormatter)
 
 parser.add_argument('-x', '--lons', type=float, nargs='+',
-                   help='List of longitudes. Ex: [-44 -45.3 -41]')
+                    help='List of longitudes. Ex: [-44 -45.3 -41]')
 
 parser.add_argument('-y', '--lats', type=float, nargs='+',
-                   help='List of latitudes: Ex: [-23 -34 -28]')
-
-parser.add_argument('-r', '--resolution', type=str, default='i',
-                   help='Coastline resolution: Ex: [i, h, f, l]')
+                    help='List of latitudes: Ex: [-23 -34 -28]')
 
 args = parser.parse_args()
 print("\n")
@@ -27,10 +25,8 @@ print("\n")
 m = folium.Map(location=[np.mean(args.lats), np.mean(args.lons)], zoom_start=2)
 
 for lon, lat in zip(args.lons, args.lats):
-    folium.Marker( location=[lat, lon]).add_to(m)
+    folium.Marker(location=[lat, lon]).add_to(m)
 
 m.save("/tmp/map.html")
 
 os.system("google-chrome /tmp/map.html")
-
-
