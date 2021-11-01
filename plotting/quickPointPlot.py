af7e84d4e306df2ee3f83389fcba6b8e890a6070
Rafael Soutelino <r.soutelino@metocean.co.nz>
2021-11-02 10:32:15 +1300

    improved help message for quickPointPlot

--- plotting/quickPointPlot.py
+++ plotting/quickPointPlot.py
@@ -5,19 +5,25 @@ import numpy as np
 import folium
 
 
-class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
+class CustomFormatter(
+    argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter
+):
     """Hack to allow showing default values in help and preserving line break in epilog"""
+
     pass
 
 
-parser = argparse.ArgumentParser(description='Currents validation.',
-                                 formatter_class=CustomFormatter)
+parser = argparse.ArgumentParser(
+    description="Currents validation.", formatter_class=CustomFormatter
+)
 
-parser.add_argument('-x', '--lons', type=float, nargs='+',
-                    help='List of longitudes. Ex: [-44 -45.3 -41]')
+parser.add_argument(
+    "-x", "--lons", type=float, nargs="+", help="List of longitudes. Ex: -44 -45.3 -41"
+)
 
-parser.add_argument('-y', '--lats', type=float, nargs='+',
-                    help='List of latitudes: Ex: [-23 -34 -28]')
+parser.add_argument(
+    "-y", "--lats", type=float, nargs="+", help="List of latitudes: Ex: -23 -34 -28"
+)
 
 args = parser.parse_args()
 print("\n")
